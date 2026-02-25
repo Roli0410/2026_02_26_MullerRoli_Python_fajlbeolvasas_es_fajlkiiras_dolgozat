1e22bbea4e8ae9a61e85ff0b123ece0a8ab1ba93
Roli0410 <muller.gabor.roland@diak.szbi-pg.hu>
2026-02-25 09:40:54 +0100

    keszdoga
    6-oson kívül megvan az összes feladat

--- forma1.py
+++ forma1.py
@@ -13,10 +13,67 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
 
+versenyzok= []
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        versenyzo = {'név': adatok[0], 'csapat': adatok[1], 'győzelmek_száma': int(adatok[2]), "teljesített_futamok_száma" : int(adatok[3]) }
+        versenyzok.append(versenyzo)
 
-print("1. A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("2. A legtöbb futamot nyert versenyző: ____")
-print("3. A legkevesebb futamot nyert versenyző: ____")
-print("4. A legtöbb futamot teljesített versenyző: ____")
-print("5. Az átlagos futamszám: ____")
-print("***A legtöbb futamgyőzelmet szerző csapat: ____")
+# print(forma1_versenyzok)
+
+#1. feladat
+versenyzok_szama = len(versenyzok)
+
+# 2. feladat
+legtobb_futamgyozelem = versenyzok[0] 
+for versenyzo in versenyzok: 
+    if versenyzo["győzelmek_száma"] > legtobb_futamgyozelem["győzelmek_száma"]: 
+        legtobb_futamgyozelem = versenyzo
+
+# 3. feladat
+legkevesebb_futamgyozelem= versenyzok[0]
+for versenyzo in versenyzok: 
+    while  versenyzo["győzelmek_száma"] < legkevesebb_futamgyozelem["győzelmek_száma"]: 
+        legkevesebb_futamgyozelem = versenyzo
+
+# 4. feladat
+legtobb_futam = versenyzok[0]
+for versenyzo in versenyzok: 
+    if versenyzo["teljesített_futamok_száma"] > legtobb_futam["teljesített_futamok_száma"]: 
+        legtobb_futam = versenyzo
+
+# 5. feladat
+osszes_futam = 0
+for versenyzo in versenyzok: 
+    osszes_futam += versenyzo["teljesített_futamok_száma"]
+atlag = osszes_futam / len(versenyzok)
+
+
+# 6. feladat
+# csapatok_gyozelme = {}
+# for versenyzo in versenyzok: 
+#     csapat = versenyzo["csapat"]
+#     gyozelmek = versenyzo["győzelmek_száma"]
+
+#     if csapat in csapatok_gyozelme:
+#         gyozelmek += csapatok_gyozelme
+#     else: 
+#         gyozelmek = csapatok_gyozelme
+# legtobb_csapat = ""
+# legtobb_gyozelem = 0
+
+# for csapat, gyozelem in csapatok_gyozelme.items():
+#     if gyozelem > legtobb_gyozelem:
+#         legtobb_csapat = csapat
+#         legtobb_gyozelem = gyozelem
+
+# sajnos nem sikerült
+
+print(f"1. A beolvasott fájlban összesen {versenyzok_szama} versenyző szerepel.")
+print(f"2. A legtöbb futamot nyert versenyző: {legtobb_futamgyozelem ["név"]}")
+print(f"3. A legkevesebb futamot nyert versenyző: {legkevesebb_futamgyozelem ["név"]  }")
+print(f"4. A legtöbb futamot teljesített versenyző: {legtobb_futam ["név"]}")
+print(f"5. Az átlagos futamszám: {round(atlag, 2)}")
+# print(f"6. A legtöbb futamgyőzelmet szerző csapat: {legtobb_csapat}")
